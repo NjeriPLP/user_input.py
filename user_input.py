8fddbfad910ae9ae40d2352fd2a8a3e52f9f724c
Susan Mwangi <suemwangi4@gmail.com>
2024-02-27 12:41:05 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,8 @@
+#ask for the user's name
+name = input()
+#ask for the user's age
+age = input()
+#ask for the user's location
+location = input()
+#print personalized message
+print("Hello Susan, you are 33 years old and you live in Nairobi")
